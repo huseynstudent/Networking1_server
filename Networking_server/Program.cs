6a1b9ba06f1e6761a36ebd72a1a5e23ef1a8c324
karim_ha78 <karim_ha78@STHQ0128-08>
2025-10-05 21:21:41 +0400

    Add project files.

---
+++ Networking_server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var port = 45678;
+var ip = IPAddress.Any;
+var localEP = new IPEndPoint(ip, port);
+
+var server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+server.Bind(localEP);
+
+Console.WriteLine($"UDP Server hazir: {localEP}");
+Console.WriteLine("Mesaj gцzl?nilir...");
+
+var buffer = new byte[1024];
+
+while (true)
+{
+    EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+    int len = server.ReceiveFrom(buffer, ref remoteEP);
+
+    var msg = Encoding.UTF8.GetString(buffer, 0, len);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"[{remoteEP}] ? {msg}");
+    Console.ResetColor();
+}
